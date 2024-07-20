0e2be84dc2f7f43f98741b4ce13a8e40de1f301a
tihitina1621 <titawondim@gmail.com>
2024-07-20 23:09:54 +0300

    magis mthods are used

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,8 +4,13 @@ class Book:
         self.author = author
         self.year = year
     def __del__(self):
-        print(f"Deleting {self.tile}")
+        print(f"Deleting {self.title}")
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
+
+my_book = Book("1984", "George Orwell", 1949)
+print(my_book)  # Expected to use __str__
+print(repr(my_book))  # Expected to use __repr__
+del my_book
