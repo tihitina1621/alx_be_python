58f2044cd8b2b3686361927c282614e9a6455116
tihitina1621 <akirasantiago106@gmail.com>
2024-07-21 18:03:39 +0300

    Update and rename Class_Static_methods_demo.py to class_Static_methods_demo.py
    new change add or multiply

--- oop/class_Static_methods_demo.py
+++ oop/class_Static_methods_demo.py
@@ -9,8 +9,3 @@ class Calculator:
         cls.b = b
         print(f'Calcualtion type: {cls.calculation_type}')
         return a * b
-sum_result = Calculator.add(10, 5)
-print(f"The sum is: {sum_result}")
-# Using the class method
-product_result = Calculator.multiply(10, 5)
-print(f"The product is: {product_result}")
