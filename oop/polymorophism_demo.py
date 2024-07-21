ef0676ded3c178b0c3256a6f969ba67bbca9957f
tihitina1621 <titawondim@gmail.com>
2024-07-21 15:40:36 +0300

    new area for rectangle and circle

---
+++ oop/polymorophism_demo.py
@@ -0,0 +1,22 @@
+class Shape:
+    def area(self):
+        pass
+class Rectangle(Shape):
+    def __init__(self,length, width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length*self.width
+import math
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius =  radius
+    def area(self):
+        return math.pi * self.radius*self.radius
+shapes = [Rectangle(10, 5),
+        Circle(7)]
+
+for shape in shapes:
+    print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+    
+
