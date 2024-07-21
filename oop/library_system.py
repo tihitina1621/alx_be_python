f1b0368dcf347eeb75e8e53155283c4e4409448a
tihitina1621 <akirasantiago106@gmail.com>
2024-07-21 23:27:26 +0300

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -14,7 +14,7 @@ class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
-    def __str__(self):
+    def asd(self):
         print(f'PrintBook: {self.title} by {self.author}, page count: {self.page_count}')
 
 class Library:
