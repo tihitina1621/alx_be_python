bffa22f38e746fda56dc4d1c34c93cbfeb284a33
tihitina1621 <titawondim@gmail.com>
2024-07-21 13:15:50 +0300

    the finale ckecker

--- oop/book_class.py
+++ oop/book_class.py
@@ -9,8 +9,18 @@ class Book:
         return f"{self.title} by {self.author}, published in {self.year}"
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
+def main():
+    # Creating an instance of Book
+    my_book = Book("1984", "George Orwell", 1949)
 
-my_book = Book("1984", "George Orwell", 1949)
-print(my_book)  # Expected to use __str__
-print(repr(my_book))  # Expected to use __repr__
-del my_book
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
+
+    # Demonstrating the __repr__ method
+    print(repr(my_book))  # Expected to use __repr__
+
+    # Deleting a book instance to trigger __del__
+    del my_book
+
+if __name__ == "__main__":
+    main()   
