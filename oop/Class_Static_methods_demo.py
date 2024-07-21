19a6bfcccc991472164fcbb6d0e237c14560534b
tihitina1621 <titawondim@gmail.com>
2024-07-21 17:23:27 +0300

    addition and mulitplication operations

---
+++ oop/Class_Static_methods_demo.py
@@ -0,0 +1,12 @@
+class Calculator:
+    calculation_type = 'Arithmetic Operations'
+    @staticmethod
+    def add(a,b):
+        return a+b
+    @classmethod
+    def multiply(cls,a,b):
+        cls.a = a
+        cls.b = b
+        print(f'Calcualtion type: {cls.calculation_type}')
+        return a * b
+ 
