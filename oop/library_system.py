550d9bc449dffa6cca80b3af9aa3b172da2ca2e5
tihitina1621 <titawondim@gmail.com>
2024-07-21 10:54:50 +0300

    amother magic module is added

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,13 +2,17 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+    def __str__(self):
+        print(f'Book: {self.title} by {self.author}')
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title)
         super().__init__(author)
         self.file_size = file_size
+    def new(self):
+        print(f'EBook: {self.title} by {self.author}, File Size: {self.file_size}')        
 class PrintBook(Book):
-    def __init__(self, title, author, page_count)
+    def __init__(self, title, author, page_count):
         super().__init__(title)
         super().__init__(author)
         self.page_count = page_count
@@ -17,3 +21,6 @@ class Library:
     def add_book(self, book):
         self.book = book
     def list_books(self):
+
+    def __str__(self):
+        return vnk'
