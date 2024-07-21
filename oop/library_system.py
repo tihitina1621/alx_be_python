4f8aedf73e7fea95595befdb03a0508df2c17eb2
tihitina1621 <akirasantiago106@gmail.com>
2024-07-21 23:12:35 +0300

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -19,27 +19,13 @@ class PrintBook(Book):
 
 class Library:
     def add_book(self, book):
-        self.book = []
+        self.books = []
         
         
-        self.book.append(book)
+        self.books.append(book)
         return book
     def list_books(self):
         for i in self.book:
             print(i)
 
-my_library = Library()
-
-    # Create instances of each type of book
-classic_book = Book("Pride and Prejudice", "Jane Austen")
-digital_novel = EBook("Snow Crash", "Neal Stephenson", '500')
-paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", '234')
-
-    # Add books to the library
-my_library.add_book(classic_book)
-my_library.add_book(digital_novel)
-my_library.add_book(paper_novel)
-
-    # List all books in the library
-my_library.list_books()
 
