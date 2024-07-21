e15e7b42704598916bd036462ce84ded8519c2f8
tihitina1621 <akirasantiago106@gmail.com>
2024-07-21 23:31:50 +0300

    Update library_system.p

--- oop/library_system.py
+++ oop/library_system.py
@@ -8,13 +8,13 @@ class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
-    def __str__(self):
+    def asd(self):
         print(f'EBook: {self.title} by {self.author}, File Size: {self.file_size}')        
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
-    def asd(self):
+    def __str__(self):
         print(f'PrintBook: {self.title} by {self.author}, page count: {self.page_count}')
 
 class Library:
