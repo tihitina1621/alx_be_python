c27c457dcf18492506dae4830a8a9e63dc021594
tihitina1621 <titawondim@gmail.com>
2024-07-21 13:04:59 +0300

    process the addition af instances

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,21 +6,32 @@ class Book:
         print(f'Book: {self.title} by {self.author}')
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        super().__init__(title)
-        super().__init__(author)
+        super().__init__(title, author)
         self.file_size = file_size
-    def new(self):
-        print(f'EBook: {self.title} by {self.author}, File Size: {self.file_size}')        
+    #def __str__(self):
+     #   print(f'EBook: {self.title} by {self.author}, File Size: {self.file_size}')        
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        super().__init__(title)
-        super().__init__(author)
+        super().__init__(title, author)
         self.page_count = page_count
 class Library:
-
-    def add_book(self, book):
+    def __init__(self, books):
+        self.books = books
+    def add_book(self,book):
         self.book = book
-    def list_books(self):
+        book = []
+        book.append(self.title.add_book(), self.author.add_book(), self.file_size.add_book(), self.page_count.add_book())
+        return book
+    #def list_books(self):
+my_library = Library('numb')
+classic_book = Book("Pride and Prejudice", "Jane Austen")
+digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
+paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
 
-    def __str__(self):
-        return vnk'
+    # Add books to the library
+my_library.add_book(classic_book)
+my_library.add_book(digital_novel)
+my_library.add_book(paper_novel)
+
+print(add_book)    
+   #library_system.py doesn't contain: ["self.books = []", "append"]
