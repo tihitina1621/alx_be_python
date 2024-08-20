691e1387eb973cda14e88a2359a62eb90ffdab53
tihitina1621 <titawondim@gmail.com>
2024-08-21 02:27:11 +0300

    variable corretion

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
-number_1 = 10 
-number_2 = 5
-sum =  number_1+number_2
-diffrence = number_1-number_2
-multiplication = number_1*number_2
-print(f'Addition of {number_1} and {number_2} is {sum}')
-print(f'Subtraction of {number_1} and {number_2} is {diffrence}')
-print(f'Multiplication of {number_1} and {number_2} is {multiplication}')
+number1 = 10 
+number2 = 5
+sum =  number1+number2
+diffrence = number1-number2
+multiplication = number1*number2
+print(f'Addition of {number1} and {number2} is {sum}')
+print(f'Subtraction of {number1} and {number2} is {diffrence}')
+print(f'Multiplication of {number1} and {number2} is {multiplication}')
