dddc620a1d1287751b33cb7cc71f0f1f833705d1
tihitina1621 <titawondim@gmail.com>
2024-09-25 12:07:01 +0300

    new

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,11 +2,9 @@ class BankAccount:
     def __init__ (self,account_balance):
         self.account_balance = account_balance
         account_balance = 0
-    def deposit(self, amount):
-        self.account_balance = account_balance
-        account_balance = account_balance + amount
+    def deposit(self, amount):        
+        self.account_balance =+ amount
     def withdraw(self, amount):
-        self.account_balance = account_balance
-        account_balance -= amount
+        self.account_balance -= amount
     def display_balance(self):
         print('Current Balance: ',self.account_balance)
