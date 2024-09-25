9cee72ae7c8af9358bbefe477fed142943411962
tihitina1621 <titawondim@gmail.com>
2024-09-25 16:05:12 +0300

    'proot'

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,6 +1,6 @@
 def safe_divide(numerator, denominator):
     try:
-        numerator // denominator
+        float(numerator) // float(denominator)
     except ZeroDivisionError:
         print('we cannot devide number by zero')
     except ValueError:
