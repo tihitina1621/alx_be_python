1aee433537f3351fddb7fd9873b670271a5e946e
tihitina1621 <titawondim@gmail.com>
2024-09-25 16:07:38 +0300

    'new'

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,7 +2,7 @@ def safe_divide(numerator, denominator):
     try:
         float(numerator) // float(denominator)
     except ZeroDivisionError:
-        print('we cannot devide number by zero')
+        print('Error: Cannot divide by zero.')
     except ValueError:
         print('Please enter only floats.')
     else:
