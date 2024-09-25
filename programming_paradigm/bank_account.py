97cdb0af59e63939b10ebc1e8fdd190e223e2eb3
tihitina1621 <titawondim@gmail.com>
2024-09-25 11:52:26 +0300

    name

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -4,7 +4,7 @@ class BankAccount:
         account_balance = 0
     def deposit(self, amount):
         account_balance = account_balance + amount
-    def withdaraw(self, amount):
+    def withdraw(self, amount):
         account_balance -= amount
     def diaplay_balace():
         print('Your current balance is ',account_balance)
