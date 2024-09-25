4365ae33c28ae2b87350525c6d5304cc12116dd1
tihitina1621 <titawondim@gmail.com>
2024-09-25 11:48:49 +0300

    encapculation

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,9 @@
+class BankAccount():
+    def __init__ (self,account_balance):
+        self.account_balance = account_balance
+    def deposit(self, amount):
+        account_balance = account_balance + amount
+    def withdaraw(self, amount):
+        account_balance -= amount
+    def diaplay_balace():
+        print('Your current balance is ',account_balance)
