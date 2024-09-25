c246b9bcb75204cb048e204ff72b434fb9504df7
tihitina1621 <titawondim@gmail.com>
2024-09-25 11:51:35 +0300

    main

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,7 @@
 class BankAccount:
     def __init__ (self,account_balance):
         self.account_balance = account_balance
+        account_balance = 0
     def deposit(self, amount):
         account_balance = account_balance + amount
     def withdaraw(self, amount):
