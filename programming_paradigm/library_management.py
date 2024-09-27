f9e999b9b3d5c2414425f87ccf03520ee6adde62
tihitina1621 <titawondim@gmail.com>
2024-09-27 13:31:19 +0300

    fdf

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__(self, title, author, _is_checked_out):
+    def _init_(self, title, author, _is_checked_out):
         self.title = title
         self.author = author
         self._is_checked_out = _is_checked_out
